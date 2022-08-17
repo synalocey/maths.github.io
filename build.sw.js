bcfad2ba66cac9b77f3ae47beb75f9574b863113
synalocey <32924919+synalocey@users.noreply.github.com>
2022-08-17 18:10:15 +0800

    Update build.sw.js

--- build.sw.js
+++ build.sw.js
@@ -3,9 +3,9 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox
 
 // 注册成功后要立即缓存的资源列表
 workbox.precaching.precacheAndRoute([
-//  {
-  //  "url": "index.html",
-  //  "revision": "220818"
+  {
+    "url": "index.html",
+    "revision": "220818"
 //  },
 //  {
   //  "url": "Guidebook_for_Mathematics.html",
@@ -18,7 +18,7 @@ workbox.precaching.precacheAndRoute([
 //  {
   //  "url": "Country_List.html",
   //  "revision": "220818"
-//  }
+  }
 ]);
 
 // 缓存策略
