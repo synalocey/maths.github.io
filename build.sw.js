992330f21b5944f55343e780beb7e82234328ffc
synalocey <32924919+synalocey@users.noreply.github.com>
2022-08-17 15:58:48 +0800

    Add files via upload

---
+++ build.sw.js
@@ -0,0 +1,45 @@
+// 首先引入 Workbox 框架
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js');
+
+// 注册成功后要立即缓存的资源列表
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "css/index.css",
+    "revision": "835ba5c3"
+  },
+  {
+    "url": "images/xxx.png",
+    "revision": "b1537bfs"
+  },
+  {
+    "url": "index.html",
+    "revision": "b331f695"
+  },
+  {
+    "url": "js/index.js",
+    "revision": "4d562866"
+  }
+]);
+
+// 缓存策略
+workbox.routing.registerRoute(
+  new RegExp('.*\.html'),
+  workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.(?:js|css)'),
+  workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://your\.cdn\.com/'),
+  workbox.strategies.staleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://your\.img\.cdn\.com/'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'example:img'
+  })
+);
